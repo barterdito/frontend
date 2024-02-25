672ad1634bfc2cbf9f958a56f3392a512d9b8d23
jpatreal <patrealmargaliti1122@gmail.com>
2024-02-25 19:33:48 +0800

    initial commit front landing page

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+    <v-app>
+      <v-main>
+        <!-- <v-app-bar :elevation="2">
+          <template v-slot:prepend>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          </template>
+          <v-app-bar-title>Application Bar</v-app-bar-title>
+        </v-app-bar> -->
+        <NuxtPage />
+      </v-main>
+    </v-app>
+  </NuxtLayout>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <div class="background-landing" :style="{backgroundImage: `url($ {image})`}"> -->
+    <img v-if="mdAndUp" class="background-landing" src="~/assets/bg-1920.jpg" />
+    <img v-else class="background-landing " src="~/assets/bg-mobile.jpg" />
+  <!-- </div> -->
+</template>
+
+<script setup>
+  import { useDisplay } from 'vuetify'
+
+  // Destructure only the keys you want to use
+  const { xs, mdAndUp } = useDisplay()
+</script>
+
+<style scoped>
+.background-landing {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size:cover;
+  background-position: center;
+}
+
+</style>
